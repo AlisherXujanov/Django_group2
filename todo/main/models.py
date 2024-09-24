0ba11027b75bf2bb26a8baff53a71d9a60853e75
Alisher <alisherxujanov163@gmail.com>
2024-09-24 17:49:57 +0500

    extended and included base html files

--- todo/main/models.py
+++ todo/main/models.py
@@ -5,11 +5,12 @@ from django.db import models
 class Todos(models.Model):
     title = models.CharField(max_length=50) # Название задачи
     owner = models.CharField(max_length=50) # Владелец задачи
+    description = models.TextField() # Описание задачи
     deadline = models.DateField() # Дедлайн задачи (время выполнения)
     completed = models.BooleanField(default=False) # Статус задачи (выполнена/нет)
     # ------------------- Время создания и обновления задачи -------------------
-    create_at = models.DateTimeField(auto_now_add=True) # Время создания задачи
-    update_at = models.DateTimeField(auto_now=True) # Время последнего обновления задачи
+    created_at = models.DateTimeField(auto_now_add=True) # Время создания задачи
+    updated_at = models.DateTimeField(auto_now=True) # Время последнего обновления задачи
     # ------------------- Дополнительные поля -------------------
     color = models.CharField(max_length=20, default='black') # Цвет задачи
     background_color = models.CharField(max_length=20, default='#f8f9fa') # Цвет фона задачи
--- todo/main/views.py
+++ todo/main/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from .models import Todos
+
+
+# CRUD - Create, Read, Update, Delete
+
 
 # Create your views here.
 def home_page(request):
+    todos = Todos.objects.all()
+
     context = {
-        "title": "Welcome to the Home Page"
+        "title": "Welcome to the Home Page",
+        "todo": todos.first()
     }
     return render(request, 'home.html', context)
+
+
+def about_page(request):
+    context = {
+        "title": "Welcome to the About Page",
+    }
+    return render(request, 'about.html', context)
