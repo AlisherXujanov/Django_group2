9ae3b0a35accbc66d4bbf5dcc0e39821da663269
Alisher <alisherxujanov163@gmail.com>
2024-10-08 17:21:41 +0500

    fixed deadline of the todo

--- todo/main/models.py
+++ todo/main/models.py
@@ -3,17 +3,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Todos(models.Model):
-    title = models.CharField(max_length=50) # Название задачи
+    title = models.CharField(max_length=50, help_text="Название задачи") # Название задачи
     owner = models.ForeignKey(User, on_delete=models.CASCADE) # Владелец задачи
-    description = models.TextField() # Описание задачи
-    deadline = models.DateTimeField() # Дедлайн задачи (время выполнения)
-    completed = models.BooleanField(default=False) # Статус задачи (выполнена/нет)
+    description = models.TextField(help_text="Описание задачи") # Описание задачи
+    deadline = models.CharField(max_length=20, help_text="Дедлайн задачи (время выполнения)") # Дедлайн задачи (время выполнения)
+    completed = models.BooleanField(default=False, help_text="Статус задачи (выполнена/нет)") # Статус задачи (выполнена/нет)
     # ------------------- Время создания и обновления задачи -------------------
     created_at = models.DateTimeField(auto_now_add=True) # Время создания задачи
     updated_at = models.DateTimeField(auto_now=True) # Время последнего обновления задачи
     # ------------------- Дополнительные поля -------------------
-    color = models.CharField(max_length=20, default='black') # Цвет задачи
-    background_color = models.CharField(max_length=20, default='#f8f9fa') # Цвет фона задачи
+    color = models.CharField(max_length=20, default='black', help_text="Цвет текста задачи") # Цвет текста
+    background_color = models.CharField(max_length=20, default='#f8f9fa', help_text="Цвет фона задачи") # Цвет фона
 
 
     class Meta:
